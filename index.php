8cc7d8988b1ebf6d073e3ac9759b46486d752a23
reistence <gvlamberti@gmail.com>
2022-11-29 18:24:31 +0100

    cleaner version converting char arrays into str

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
 <?php 
 session_start();
-
-include_once __DIR__ . "/partials/function.php";
-
-// $pssw_len = intval($_GET["pass-len"] ?? '');
-
-// $aplhabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// $special_characters = ['!', '£', '$', '%', '&','/', '(', ')', '=', '?', '^', '*', '§', '.', ':', ';', ',', '@', '#', '_']; 
-
-// $numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// $all_characters = array_merge($aplhabet, $special_characters, $numbers);
-
-// $generated_pssw = genratePassword($pssw_len, $all_characters);
-
-// if (!empty($generated_pssw)) {
-//     $_SESSION["generated_pssw"] = $generated_pssw;
-//     var_dump($_SESSION);
-// };
-
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -33,38 +12,33 @@ include_once __DIR__ . "/partials/function.php";
 </head>
 <body>
     <div class="container">
-
-        <div>
-            
+        <div>  
             <h1>Password Generator</h1>
             <h2>Generate a secure password</h2>
-            </div>
+        </div>
         <form action="generated_password.php" method="GET">
-        <div>
-
-            <label for="pssw-len">Password length:</label>
-            <input type="text" id="pssw-len" name="pass-len">
-        </div>
-        <div>
-
-            <input type="checkbox" name="letters" id="letters" checked>
-            <label for="letters">Letters</label>
-        </div>
-        <div>
-
-            <input type="checkbox" name="numbers" id="numbers" checked>
-            <label for="numbers">Numbers</label>
-        </div>
-        <div>
-
-            <input type="checkbox" name="symbols" id="symbols" checked>
-            <label for="symbols">Symbols</label>
-
-        </div>
-        <button type="submit">Submit</button>
-    </form>
+            <div>
+                <label for="pssw-len">Password length:</label>
+                <input type="text" id="pssw-len" name="pass-len">
+            </div>
+             <div>
+                 <input type="checkbox" id="repetition" name="repetition">
+                 <label for="repetition">Allow repetition</label>
+            </div>
+            <div>
+                <input type="checkbox" name="letters" id="letters" checked>
+                <label for="letters">Letters</label>
+            </div>
+            <div>
+                <input type="checkbox" name="numbers" id="numbers" checked>
+                <label for="numbers">Numbers</label>
+            </div>
+            <div>
+                <input type="checkbox" name="symbols" id="symbols" checked>
+                <label for="symbols">Symbols</label>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
     </div>
-
-    
 </body>
 </html>
--- partials/function.php
+++ partials/function.php
@@ -1,27 +1,28 @@
 <?php 
 
-function genratePassword($user_length, $array){
+function genratePassword($user_length, $string, $boolean){
     $password_string = '';
-    $array_length = count($array) - 1;
-    while (strlen($password_string) < $user_length ) {
-        $password_string .= $array[rand(0,$array_length)];    
+    //ARRAY VERSION
+    // $array_length = count($array) - 1; 
+    //STRING VERSION
+    $string_length = strlen($string) - 1;
+
+    if($user_length >= 8){
+        while (strlen($password_string) < $user_length ) {
+            if(!$boolean){
+                if(!str_contains($password_string, $string[rand(0,$string_length)] )){
+                    $password_string .= $string[rand(0,$string_length)];    
+                };
+    
+            } else {
+                $password_string .= $string[rand(0,$string_length)];    
+            }
+        }
+    } else {
+        $password_string = "The Password length must be grater than 8 characters";
     }
     return $password_string;
 };
 
-
-// function genratePasswordWithoutTwins($user_length, $array){
-//     $new_pass = "";
-//     $array_length = count($array) - 1;
-   
-//         do {
-//          $new_pass .= $array[rand(0,$array_length - 1)];
-//         } while ( strlen($new_pass) < $user_length && str_contains($new_pass, $array[rand(0,$array_length)] ) === false);
-//         // var_dump($new_pass);
-
-//     return $new_pass;
-    
-
-// }
 
 ?>
